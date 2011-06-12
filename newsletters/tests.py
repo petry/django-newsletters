ae8f23653527a86ddeaeaff45fbebdb78c0c8c66
Marcos Daniel Petry <marcospetry@gmail.com>
2011-06-11 22:16:47 -0300

    added subscription test

--- newsletters/tests.py
+++ newsletters/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+from newsletters.models import Subscription
 
 class RequestTest(TestCase):
     def setUp(self):
@@ -23,12 +24,32 @@ class RequestTest(TestCase):
 
 
     def test_subscription_activate_view_works(self):
-        response = self.client.get(reverse('subscription-activate', kwargs={'activation_key':000}))
+        response = self.client.get(reverse('subscription-activate',
+                                        kwargs={'activation_key':000}))
         self.assertEqual(response.status_code, 200)
 
 
     def test_subscription_deactivate_view_works(self):
-        response = self.client.get(reverse('subscription-deactivate', kwargs={'deactivation_key':000}))
+        response = self.client.get(reverse('subscription-deactivate',
+                                           kwargs={'deactivation_key':000}))
         self.assertEqual(response.status_code, 200)
 
-    
+
+class SubscriptionTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+
+    def test_subscription_add_an_email(self):
+        response = self.client.post(reverse('subscribe'),
+                                    data={'email':'test@test.com'})
+        self.assertEqual(Subscription.objects.count(), 1)
+
+
+    def test_only_one_email_are_bubscribed(self):
+        Subscription.objects.create(email="test@test.com")
+        response = self.client.post(reverse('subscribe'),
+                                        data={'email':'test@test.com'})
+
+        form = response.context['form']
+        self.assertEqual(form.errors['email'], [u'E-mail already registered'])
